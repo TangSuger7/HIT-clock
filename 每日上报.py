0187551d3149839df2e274be239cb1205e8da744
TangSuger7 <76415982+TangSuger7@users.noreply.github.com>
2022-01-27 19:03:29 +0800

    Update 每日上报.py

--- 每日上报.py
+++ 每日上报.py
@@ -1,13 +1,13 @@
 import os
 import traceback
-import time
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 
 print('初始化浏览器')
-ID   = os.environ['ID']
-key   = os.environ['PASSWORD']
-loc   = os.environ['LOCATION']
+USERNAME   = os.environ['ID']
+PASSWORD   = os.environ['PASSWORD']
+LOCATION   = os.environ['LOCATION']
 ua = 'Mozilla/5.0 (iPhone; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Mobile/14A403 NetType/WIFI Language/zh_CN'
 app = 'HuaWei-AnyOffice/1.0.0/cn.edu.hit.welink'
 option = webdriver.ChromeOptions()
@@ -16,66 +16,41 @@ option.add_argument('user-agent='+ua)
 driver = webdriver.Chrome(executable_path= '/usr/bin/chromedriver', options = option)
 
 print('正在上报')
+driver.get('https://ids.hit.edu.cn/authserver/login')
+driver.find_element_by_id('username').send_keys(USERNAME)
+driver.find_element_by_id('password').send_keys(PASSWORD)
+driver.find_element_by_id('login_submit').click()
 
-driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew/edit')
-if driver.find_element_by_id('username'):
-    time.sleep(2)
-    driver.find_element_by_id('username').send_keys(ID)
-#     driver.find_element_by_id('rememberMe').click()
-    driver.find_element_by_id('password').send_keys(key)
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": ua + ' ' + app})
 
-# driver.find_element_by_id('login_submit').submit()
-time.sleep(1)
+def tryClick(id):
+	try:
+		driver.execute_script(f'document.getElementById("{id}").click()')
+	except:
+		print(f'No such checkbox: {id}')
+		pass
 
-driver.execute_script(f'kzl10 = "{loc}"')
-driver.execute_script('document.getElementsByClassName("checkbtn")[0].checked=true')
-driver.execute_script('document.getElementsByClassName("checkbtn")[1].checked=true')
-driver.execute_script('document.getElementsByClassName("checkbtn")[2].checked=true')
-# driver.execute_script('document.getElementsByClassName("checkbtn")[0].click()')
-# driver.execute_script('document.getElementsByClassName("checkbtn")[1].click()')
-# driver.execute_script('document.getElementsByClassName("checkbtn")[2].click()')
+driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": ua + ' ' + app})
 
-driver.find_element_by_class_name('submit').click()
-time.sleep(1)
-
-try:
-    driver.find_element_by_class_name('weui-toptips.bg-error.weui-toptips_visible')
-except:
-    print('success!')
-
-# driver.get('https://ids.hit.edu.cn/authserver/')
-# driver.find_element_by_id('mobileUsername').send_keys(USERNAME)
-# driver.find_element_by_id('mobilePassword').send_keys(PASSWORD)
-# driver.find_element_by_id('load').click()
-
-# driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": ua + ' ' + app})
-
-# def tryClick(id):
-# 	try:
-# 		driver.execute_script(f'document.getElementById("{id}").click()')
-# 	except:
-# 		print(f'No such checkbox: {id}')
-# 		pass
-
-# success = False
-# for i in range (0, 5):
-# 	try:
-# 		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew/edit')
-# 		driver.execute_script(f'kzl10 = "{LOCATION}"')
+success = False
+for i in range (0, 5):
+	try:
+		driver.get('https://xg.hit.edu.cn/zhxy-xgzs/xg_mobile/xsMrsbNew/edit')
+		driver.execute_script(f'kzl10 = "{LOCATION}"')
 # 		driver.execute_script('document.getElementById("kzl18-0").checked = true')
 # 		driver.execute_script('document.getElementById("kzl32-2").checked = true')
-# 		tryClick("txfscheckbox")
-# 		tryClick("txfscheckbox1")
-# 		tryClick("txfscheckbox2")
-# 		tryClick("txfscheckbox3")
-# 		driver.find_element_by_class_name('submit').click()
-# 		success = True
-# 		break
-# 	except:
-# 		traceback.print_exc()
-# 		print('失败' + str(i+1) + '次，正在重试...')
-# driver.quit()
-# if success:
-# 	print('上报完成')
-# else:
-# 	raise Exception('上报多次失败，可能学工系统已更新')
+		tryClick("txfscheckbox")
+		tryClick("txfscheckbox1")
+		tryClick("txfscheckbox2")
+		tryClick("txfscheckbox3")
+		driver.find_element_by_class_name('submit').click()
+		success = True
+		break
+	except:
+		traceback.print_exc()
+		print('失败' + str(i+1) + '次，正在重试...')
+driver.quit()
+if success:
+	print('上报完成')
+else:
+	raise Exception('上报多次失败，可能学工系统已更新')
